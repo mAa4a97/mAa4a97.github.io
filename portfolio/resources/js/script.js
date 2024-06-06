76ca1b8785bd3c45b4a5d534190b4de0abc3d76c
mAa4a <zobbroslv@gmail.com>
2024-06-06 23:50:37 +0300

    Added possibility to navigate the gallery with keyboard.

--- portfolio/resources/js/script.js
+++ portfolio/resources/js/script.js
@@ -10,3 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add event listener to Socials for scrolling to the bottom
     
 });
+
+document.addEventListener('keydown', function(event) {
+    let button;
+    switch(event.key) {
+        case 'ArrowRight':
+            document.querySelector('.m-p-g__controls-arrow--next').click();
+            break;
+        case 'ArrowLeft':
+            document.querySelector('.m-p-g__controls-arrow--prev').click();
+            break;
+        case 'Escape':
+            document.querySelector('.m-p-g__controls-close').click();
+            break;
+    }
+});
